a82aba0f0a167604731dd0ac5ac8965bbb11dae7
unknown <seandanielrooney@gmail.com>
2024-11-29 18:28:05 -0800

    flkfghkfjghkdfshg

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-simple-starter.netlify.app/",
+  site: "https://fafco.net/",
   integrations: [tailwind(), sitemap()]
 });
